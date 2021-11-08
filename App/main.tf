49001feacb141ca1f47cbdef885f87cf241e0376
nsabarat <68889522+nsabarat@users.noreply.github.com>
2021-11-08 14:31:28 +0530

    Update main.tf

--- App/main.tf
+++ App/main.tf
@@ -1,1 +1,12 @@
 
+resource "aws_instance" "codefresh1" {
+  ami                   = "ami-08fff96d475023079"
+  instance_type         = "t2.micro"
+  key_name              = "TestingANZ"
+  availability_zone     = "us-east-1c"
+  associate_public_ip_address  = "true"
+    tags = {
+    name = "terragrunt1"
+	Autostop = "yes"
+  }
+}
